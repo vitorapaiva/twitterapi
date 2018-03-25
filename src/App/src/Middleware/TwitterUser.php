cb79c2cbd9e01f104040ae0302585a2c79b5e746
Vitor Albuquerque e Paiva <vitorapaiva@gmail.com>
2018-03-25 03:11:13 -0300

    Modificacoes

---
+++ src/App/src/Middleware/TwitterUser.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Middleware;
+
+use Psr\Http\Message\ServerRequestInterface;
+use App\Models\WebApi\TwitterApi;
+use Exception;
+
+class User
+{
+    protected $twitterApi;
+    protected $helper;
+
+
+    public function __construct(TwitterApi $twitterApi)
+    {
+        $this->twitterApi = $twitterApi;
+    }
+
+
+    public function process(ServerRequestInterface $request)
+    {
+        $twitter_user = $request->getParsedBody();
+        try {
+            $user_info = $this->twitterApi->getUserInfo($twitter_user);
+            var_dump($user_info);
+            return new HtmlResponse($this->template->render('app::home-page', $user_info));
+        } catch (Exception $e) {
+            return $response->withStatus(400);
+        }
+        
+    }
+}
---
+++ src/App/src/Models/WebApi/TwitterApi.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Models\WebApi;
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+
+
+class TwitterApi 
+{
+
+    private $oauth_access_token;
+    private $oauth_access_token_secret;
+    private $consumer_key;
+    private $consumer_secret;
+    
+    public function __construct(){
+        $this->oauth_access_token = "48435300-FCaFI5ArOeMWby14spFlSBmHIlpuQ1SzbyiG6BLi3";
+        $this->oauth_access_token_secret = "OA3hVHiciutspkLPs02syEDFKB242hkD5OczhFVifMviS";
+        $this->consumer_key = "LxpOVle8anNQ4xul4J7LxBzPF";
+        $this->consumer_secret = "JTphkrctVWAMrZPUtrNVH6PAHROsuszGRGbXDxXAh7pM0r9oiX";
+    }
+
+    public function connectTwitter(){
+        return new TwitterOAuth($this->consumer_key, $this->consumer_secret, $this->oauth_access_token, $this->oauth_access_token_secret);
+    }
+
+
+    public function getUserInfo($user){
+        $connection = $this->connectTwitter();
+        return $connection->get("search/tweets", ["q" => "twitterapi"]);
+    }
+}
+
+
