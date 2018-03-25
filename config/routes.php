bf473c929c5c9465d5bf84879d6857c358c347ab
Vitor Albuquerque e Paiva <vitorapaiva@gmail.com>
2018-03-25 03:16:02 -0300

    teste

--- config/routes.php
+++ config/routes.php
@@ -34,5 +34,4 @@ use Zend\Expressive\MiddlewareFactory;
  */
 return function (Application $app, MiddlewareFactory $factory, ContainerInterface $container) : void {
     $app->get('/', App\Handler\HomePageHandler::class, 'home');
-    $app->post('/', App\Middleware\TwitterUser::class, 'home');
 };
