d027e45ca2328cc51d8de412620a40de370d9146
Vitor Albuquerque e Paiva <vitorapaiva@gmail.com>
2018-03-26 08:20:48 -0300

    Ultimas modificacoes

--- config/routes.php
+++ config/routes.php
@@ -34,5 +34,5 @@ use Zend\Expressive\MiddlewareFactory;
  */
 return function (Application $app, MiddlewareFactory $factory, ContainerInterface $container) : void {
     $app->get('/', App\Handler\HomePageHandler::class, 'home');
-    $app->post('/', App\Middleware\TwitterUser::class, 'home');
+    $app->post('twitter/', App\Middleware\TwitterUser::class, 'home');
 };
